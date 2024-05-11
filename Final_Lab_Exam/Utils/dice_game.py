250dfe7414823223f68688cdb07b5846729b4922
MICHAELLA BALAGTAS <153539219+CIMALLEAH@users.noreply.github.com>
2024-05-11 11:34:52 +0800

    Add files via upload

---
+++ Final_Lab_Exam/Utils/dice_game.py
@@ -0,0 +1,25 @@
+import os
+from Utils.score import Score
+from Utils.user_manager import UserManager
+
+class DiceGame:
+    def __init__(self) -> None:
+        pass
+
+    def load_scores(self):
+        pass
+
+    def save_scores(self):
+        pass
+
+    def play_game(self):
+        pass
+
+    def show_top_scores(self):
+        pass
+
+    def logout(self):
+        pass
+
+    def menu(self):
+        pass
---
+++ Final_Lab_Exam/Utils/user_manager.py
@@ -0,0 +1,23 @@
+import os
+
+class UserManager:
+    def __init__(self) -> None:
+        pass
+
+    def load_user(self):
+        pass
+
+    def save_user(self):
+        pass
+
+    def validate_username(self):
+        pass
+
+    def validate_password(self):
+        pass
+    
+    def register(self):
+        pass
+
+    def login(self):
+        pass
