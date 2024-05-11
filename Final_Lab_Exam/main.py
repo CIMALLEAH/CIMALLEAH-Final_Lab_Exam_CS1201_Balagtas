d730a2118afe2c7d7b673ce6eeeb8bed3c54e162
MICHAELLA BALAGTAS <153539219+CIMALLEAH@users.noreply.github.com>
2024-05-11 10:14:41 +0800

    Add files via upload

---
+++ Final_Lab_Exam/main.py
@@ -0,0 +1,27 @@
+def main():
+    print ("Welcome to the Dice Roll Game ")
+    print ("1. Register")
+    print ("2. Login")
+    print ("3. Exit")
+
+    while True:
+        try:
+            choice = input ("Enter your choice: ")
+
+            if choice == '1':
+                pass
+
+            elif choice == '2':
+                pass
+
+            elif choice == '3':
+                break
+
+            else:
+                raise ValueError ("Invalid choice. Please try again.")
+        
+        except ValueError as e:
+            print (e)
+
+if __name__ == "__main__":
+    main()
