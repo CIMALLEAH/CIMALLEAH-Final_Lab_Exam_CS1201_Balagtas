68fc722935a2a1fbf981bbba628546afbe1ce67e
MICHAELLA BALAGTAS <153539219+CIMALLEAH@users.noreply.github.com>
2024-05-12 00:17:05 +0800

    Add files via upload

--- Final_Lab_Exam/main.py
+++ Final_Lab_Exam/main.py
@@ -1,20 +1,22 @@
-from Utils.dice_game import DiceGame
+from Utils.user_manager import UserManager
 
 def main():
-    print ("Welcome to the Dice Roll Game ")
-    print ("1. Register")
-    print ("2. Login")
-    print ("3. Exit")
+    user_manager = UserManager()
 
     while True:
         try:
-            choice = input ("Enter your choice: ")
+            print ("Welcome to the Dice Roll Game!")
+            print (" 1. Register")
+            print (" 2. Login")
+            print (" 3. Exit")
+            
+            choice = input ("\nEnter your choice: ")
 
             if choice == '1':
-                pass
+                user_manager.register()
 
             elif choice == '2':
-                pass
+                user_manager.login()
 
             elif choice == '3':
                 break
@@ -23,9 +25,7 @@ def main():
                 raise ValueError
         
         except ValueError as e:
-            print (f"Invalid choice. Please try again.{e}")
+            print (f"\nInvalid choice. Please try again.{e}")
 
 if __name__ == "__main__":
-    game = DiceGame()
-    game.menu()
     main()
